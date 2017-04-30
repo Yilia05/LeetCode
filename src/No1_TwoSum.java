f776e07445cea3a89b4032b1dd25637b2ab8315e
Yilia05 <532104074@qq.com>
2017-05-01 00:10:01 +0800

    No3 HashMap runnable

--- src/No1_TwoSum.java
+++ src/No1_TwoSum.java
@@ -7,7 +7,7 @@ Output: index1=1, index2=2
 */
 //my solution:
 
-public class Solution {
+public class No1_TwoSum {
     public static int[] twoSum(int[] nums, int target) {
 	        int i = 0;
 	        int j = 0;
@@ -33,18 +33,18 @@ public class Solution {
 
 //O(n) solution:using hashmap
 
-public int[] twoSum(int[] numbers, int target) {
-    int[] result = new int[2];
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for (int i = 0; i < numbers.length; i++) {
-        if (map.containsKey(target - numbers[i])) {
-            result[1] = i + 1;
-            result[0] = map.get(target - numbers[i]);
-            return result;
-        }
-        map.put(numbers[i], i + 1);
-    }
-    return result;
-}
+//public int[] twoSum(int[] numbers, int target) {
+//    int[] result = new int[2];
+//    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+//    for (int i = 0; i < numbers.length; i++) {
+//        if (map.containsKey(target - numbers[i])) {
+//            result[1] = i + 1;
+//            result[0] = map.get(target - numbers[i]);
+//            return result;
+//        }
+//        map.put(numbers[i], i + 1);
+//    }
+//    return result;
+//}
 
 
---
+++ src/No3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class No3_LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0)
+            return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
+    public static void main(String args[]) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+}
+
