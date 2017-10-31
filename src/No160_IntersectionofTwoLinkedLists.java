9cd1f3dd72ab2d8272085c9b389af77ec24fb6b8
YiliaZhang <zhangyu05cn@126.com>
2017-10-31 15:40:29 +0800

    No.160 solution

---
+++ src/No160_IntersectionofTwoLinkedLists.java
@@ -0,0 +1,54 @@
+/**
+ * Created by yzhang46 on 2017/10/31.
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ */
+public class No160_IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = length(headA), lenB = length(headB);
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        // find the intersection until end
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+
+
+    //another method
+    /**
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //boundary check
+        if(headA == null || headB == null) return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        //if a & b have different len, then we will stop the loop after second iteration
+        while( a != b){
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            a = a == null? headB : a.next;
+            b = b == null? headA : b.next;
+        }
+
+        return a;
+    }
+     */
+}
