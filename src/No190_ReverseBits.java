07a3834eb36f479d215ad6d0d1202ff87431284f
YiliaZhang <zhangyu05cn@126.com>
2017-11-09 12:52:56 +0800

    No.190 solution

---
+++ src/No190_ReverseBits.java
@@ -0,0 +1,19 @@
+/**
+ * Created by yzhang46 on 2017/11/9.
+ * Reverse bits of a given 32 bits unsigned integer.
+ * For example, given input 43261596 (represented in binary as 00000010100101000001111010011100),
+ * return 964176192 (represented in binary as 00111001011110000010100101000000).
+ */
+public class No190_ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+    }
+}
